91b8336e6893c2944696e0bb2c8ff825ec6334d9
Alan Reche <alanaxelreche@hotmail.com>
2023-03-25 20:21:03 -0300

    Modal hook + App Modal installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import Counter from "./components/Counter";
 import CounterStructured from "./components/CounterStructured";
+import Modal from "./components/Modal";
 // import { CrudApi } from "./components/CrudApi";
 import ShoppingCart from "./components/ShoppingCart";
+import { useModal } from "./hooks/useModal";
 
 function App() {
+  const [isOpen, openModal, closeModal] = useModal(false);
+
   return (
     <div>
-      <h1 style={{fontSize:"35px"}}>useReducer</h1>
+      {/* Modal */}
+      <button className="btnModal" onClick={openModal}>
+        <h1 style={{ fontSize: "35px" }}>useReducer</h1>
+      </button>
+      <Modal isOpen={isOpen} closeModal={closeModal}>
+        <div className="ExplinationModal">
+          <h3>React Reducer</h3>
+          <p style={{ textAlign: "center" }}>
+           ...
+          </p>
+          <p>
+           ...
+          </p>
+          <p>
+            ...
+          </p>
+          <p>
+            ...
+          </p>
+          <p style={{ textAlign: "center" }}>
+            ...
+          </p>
+        </div>
+      </Modal>
+      {/* Modal */}
       {/* <hr /> */}
       {/* <CrudApi /> */}
       <hr />
